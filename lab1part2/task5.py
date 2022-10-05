83e677895f1cf57d6df943d4d9868bc2a72e69f1
Oleg Ataman <101531069+OlegAtaman@users.noreply.github.com>
2022-10-05 20:02:25 +0300

    Add files via upload

---
+++ lab1part2/task5.py
@@ -0,0 +1,37 @@
+class Student:
+    def __init__(self, name, surname, booknum, grades):
+        self.name = name
+        self.surname = surname
+        self.booknum = booknum
+        self.grades = grades
+        self.average = self.avg()
+
+    def avg(self):
+        return sum(self.grades) / len(self.grades)
+
+class Group:
+    def __init__(self, students):
+        self.students = []
+        if len(students) <= 20:
+            self.students = students
+        else:
+            print('More than 20 students')
+
+    def print_top(self):
+        if self.students:
+            top_students = sorted(self.students, key=lambda x: x.average, reverse=True)[:5]
+            print('Top five students:')
+            for i in range(len(top_students)):
+                print(f'{i + 1}. {top_students[i].name} {top_students[i].surname} - {top_students[i].average} points')
+
+
+stud1 = Student('Billy', 'Herrington', 'HFDKEN', [5, 4, 5, 5])
+stud2 = Student('Van', 'Darkholme', 'FDDSGD', [5, 5, 4, 5])
+stud3 = Student('Semen', 'Vovchyk', 'DDWSDX', [4, 4, 3, 3])
+stud4 = Student('XAEA-12', '', 'EFFSEC', [5, 5, 3, 4])
+stud5 = Student('Bot', 'Henry', 'EKJFNC', [3, 5, 3, 4])
+stud6 = Student('Lackof', 'Imagination', 'DEWDDD', [4, 3, 4, 4])
+
+
+group1 = Group([stud1, stud2, stud3, stud4, stud5, stud6])
+group1.print_top()
